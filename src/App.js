8eddecfaf9fe6905f7ce6cd9cce2fa6b43581412
GikariDigital <dihuthia@gmail.com>
2025-08-04 15:47:22 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Vehicle Import Tax Calculator</h1>
+      <p>This is a placeholder for the dropdown logic.</p>
+    </div>
+  );
+}
+
+export default App;
